1c36e7e91687624458c2cb764592ad7a20c2ba76
unknown <c.davalos@demonsystem.com>
2018-03-27 11:42:49 -0600

    Change Login When a User Aren't Login Or Register

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request as IlluminateRequest;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,19 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function showLoginForm(IlluminateRequest $request){
+        if($request->has('redirect_to')){
+            session()->put('redirect_to', $request->input('redirect_to'));
+        }
+        return view('auth.login');
+    }
+
+    public function redirectTo(){
+        if(session()->has('redirect_to'))
+            return session()->pull('redirect_to');
+
+        return $this->redirectTo;
     }
 }
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -34,9 +34,15 @@
             </div>
 
             <div class="text-center">
+                @if(auth()->check())
                 <button class="btn btn-primary btn-round" data-toggle="modal" data-target="#modalAddToCart">
                     <i class="material-icons">add</i> Añadir al carrito de compras
                 </button>
+                @else
+                <a href="{{ url('/login?redirect_to='.url()->current()) }}" class="btn btn-primary btn-round">
+                    <i class="material-icons">add</i> Añadir al carrito de compras
+                </a>
+                @endif
             </div>
 
             <div class="row">
